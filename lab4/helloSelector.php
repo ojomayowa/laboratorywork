47b21ba44e891d08ebbed2b3279923bf218a0d57
ojomayowa <o.ojo2@rgu.ac.uk>
2020-02-24 13:59:20 +0000

    lab8 work

---
+++ lab4/helloSelector.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello world</title>
+</head>
+<body>
+<header>
+    <h2>Where would you like to say hello?</h2>
+</header>
+<main>
+    <ul>
+        <li><a href="helloSelector.php?planet=Earth">Earth</a></li>
+        <li><a href="helloSelector.php?planet=Mars">Mars</a></li>
+        <li><a href="helloSelector.php?planet=Uranus">Uranus</a></li>
+    </ul>
+</main>
+</body>
+</html>
---
+++ lab8/connection.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_SERVER', 'csdm-webdev');
+define('DB_USERNAME', '2000953');
+define('DB_PASSWORD', '2000953');
+define('DB_DATABASE', 'db2000953_cmm007');
+$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
